2466fd649373f2d9b86568a1afde89af6a94552b
harsha <harshavardhanreddyb07@gmail.com>
2022-08-21 09:21:02 +0530

    frebase config edits

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
 import { productData, productDataTwo } from "./components/Products/data";
-import { FireApp } from "./firebase";
 import { GlobalStyle } from "./globalStyles";
 
 function App() {
   return (
 
     <BrowserRouter>
-      <FireApp />
       <GlobalStyle />
       <Hero />
       <Products heading='Choose your favorite' data={productData} />
--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import firebase from './firebase/app';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
-firebase.initializeApp({
+
+
+const firebaseConfig = {
   apiKey: "AIzaSyDhUH2stq0gq0kZIGSZXHL1bUHLg8OD8hc",
   authDomain: "reactpizzeria.firebaseapp.com",
   projectId: "reactpizzeria",
   storageBucket: "reactpizzeria.appspot.com",
   messagingSenderId: "351598623356",
   appId: "1:351598623356:web:8b7e3257720b815e7367f9"
-});
+};
 
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <FireApp />
   </React.StrictMode>
 );
 
