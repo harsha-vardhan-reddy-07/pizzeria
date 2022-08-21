11459d236831cf01f9b98bbed4a022c2f8330e82
harsha <harshavardhanreddyb07@gmail.com>
2022-08-21 09:31:47 +0530

    hosting setup finished

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ import { initializeApp } from "firebase/app";
 
 
 
-const firebaseConfig = {
+initializeApp({
   apiKey: "AIzaSyDhUH2stq0gq0kZIGSZXHL1bUHLg8OD8hc",
   authDomain: "reactpizzeria.firebaseapp.com",
   projectId: "reactpizzeria",
   storageBucket: "reactpizzeria.appspot.com",
   messagingSenderId: "351598623356",
   appId: "1:351598623356:web:8b7e3257720b815e7367f9"
-};
+});
 
-initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
